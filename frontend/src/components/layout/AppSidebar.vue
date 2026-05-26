2da4efaa03582cfe32a07f29152c3f494ddbc7c2
KeeperD93 <keeperdfr@proton.me>
2026-05-26 17:43:22 +0200

    style(sidebar): swap Tag → Package icon on version line
    User preference — Package conveys 'release artifact / build' more
    explicitly than Tag (which can also mean label/category).

--- frontend/src/components/layout/AppSidebar.vue
+++ frontend/src/components/layout/AppSidebar.vue
@@ -178,7 +178,7 @@
     <div class="sb-footer">
       <router-link to="/changelog" class="sb-version-link" :title="$t('changelog.title')">
         <span class="sb-version">
-          <Tag v-if="!collapsed" :size="11" :stroke-width="2" class="sb-version-icon" />
+          <Package v-if="!collapsed" :size="11" :stroke-width="2" class="sb-version-icon" />
           v{{ appVersion }}
         </span>
         <span v-if="hasNewChangelog" class="sb-version-dot" />
@@ -211,7 +211,7 @@ import SidebarSection from './SidebarSection.vue'
 import { useSidebarCounters } from '@/composables/useSidebarCounters'
 import { fetchApiResponse, useApi } from '@/composables/useApi'
 import { useMobile } from '@/composables/useMobile'
-import { ChevronLeft, MessageSquare, Search, Tag } from 'lucide-vue-next'
+import { ChevronLeft, MessageSquare, Package, Search } from 'lucide-vue-next'
 import { SIDEBAR_SUB_TABS } from '@/constants/sidebarSubTabs'
 import { SIDEBAR_MODULES } from '@/constants/sidebarModules'
 
